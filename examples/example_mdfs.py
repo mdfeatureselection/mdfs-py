eda8e43a70b86de292becdcc08e43fd7304b6009
Radosław Piliszek <radoslaw.piliszek@gmail.com>
2023-10-14 21:51:09 +0200

    Add more testing material
    Confirming reproducibility of R, except for generation of
    contrast variables.
    
    Related to issues #3 and #4.

--- examples/example_mdfs.py
+++ examples/example_mdfs.py
@@ -3,17 +3,13 @@ import numpy as np
 import pandas as pd
 import json
 
+
 #| Reading data
 madelon = pd.read_csv("data/madelon.50.csv", dtype=float)
 dim = madelon.shape
 data = madelon[[col for col in madelon.columns[:dim[1]-1]]][:dim[0]].to_numpy()
 decision = madelon['Class'][:dim[0]].astype(np.intc).to_numpy() - 1
 
-dimensions = 1
-
-result = mdfs.run(data, decision, dimensions=dimensions, seed=0, discretizations=30, n_contrast=30)
-
-contrast_variables = mdfs.gen_contrast_variables(data, n_contrast=30, seed=0)
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -26,11 +22,18 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-with open("data/MDFS_Result_python_" + str(dimensions) + "D.json", "w") as f:
-    f.write(json.dumps(result, indent=4, cls=NumpyEncoder))
+def run_example_mdfs(dimensions):
+    return mdfs.run(data, decision, dimensions=dimensions, seed=0, discretizations=30, n_contrast=30)
+
+
+for dimensions in [1, 2]:
+    result = run_example_mdfs(dimensions=dimensions)
+    with open("data/MDFS_Result_python_" + str(dimensions) + "D.json", "w") as f:
+        f.write(json.dumps(result, indent=4, cls=NumpyEncoder))
 
 # for contrast variable debugging:
-# np.savetxt("data/test_contrast_variables.csv", contrast_variables.data, delimiter="\t")
+contrast_variables = mdfs.gen_contrast_variables(data, n_contrast=30, seed=0)
+np.savetxt("data/test_contrast_variables.csv", contrast_variables.data, delimiter="\t")
 
 pairs = mdfs.compute_tuples(data=data, decision=decision, seed=0, discretizations=30, return_matrix=True)
 
